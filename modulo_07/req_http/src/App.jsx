cafc7d896bf0534d56edf038423dada52cf49af6
ANDRIW AMORIM SANTAREM <70485336+andriwamorimsan@users.noreply.github.com>
2025-02-26 16:34:39 -0400

    conexao com banco via json

--- modulo_07/req_http/src/App.jsx
+++ modulo_07/req_http/src/App.jsx
@@ -8,7 +8,9 @@ const url = "http://localhost:3000/products";
 // função app
 function App() {
   // variaveis
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState([]);
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState("");
 
   //   aula 1 resgatando dados
     useEffect(() => {
@@ -26,6 +28,25 @@ function App() {
 
     }, []);
 
+  //   2- adicionar produtos
+const handleSubmit = async (e) =>{
+    e.preventDefault()
+
+    const product = {
+        name,
+        price
+    };
+
+    const res = await fetch(url,{
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(product),
+    });
+
+
+}
 
 
   // view
@@ -41,6 +62,20 @@ function App() {
               ))}
 
           </ul>
+          <div className="add-product">
+              <form onSubmit={handleSubmit}>
+                  <label>
+                      Nome:
+                      <input type="text" value={name} name="name" onChange={(e) => setName(e.target.value)}/>
+                  </label>
+                  <label>
+                      Preço:
+                      <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)}/>
+                  </label>
+                  <input type="submit" value="Criar"/>
+              </form>
+          </div>
+
       </div>
     </>
   )
